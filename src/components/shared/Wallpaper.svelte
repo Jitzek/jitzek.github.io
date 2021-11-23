b7536d5dde5b53d536177b679b9a005a293010ce
Jitzek <jitz_k@hotmail.com>
2021-11-23 13:49:58 +0100

    init

---
+++ src/components/shared/Wallpaper.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let src: string =
+    "/static/images/wallpapers/custom-design-01-1280x720.png";
+</script>
+
+<img {src} alt="wallpaper" unselectable={true} draggable={false} />
+
+<style lang="scss">
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    position: fixed;
+    z-index: -1;
+
+    // pointer-events: none;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { desktop, mobile } from "$stores/shared/DeviceTypeStore";
+  import Desktop from "$components/desktop/Desktop.svelte";
+  import { onMount } from "svelte";
+  import Mobile from "$components/mobile/Mobile.svelte";
+
+  let mounted: boolean = false;
+  onMount(() => {
+    mounted = true;
+  });
+</script>
+
+{#if mounted}
+  {#if $desktop}
+    <Desktop />
+  {:else if $mobile}
+    <Mobile />
+  {/if}
+{:else}
+  <!-- Show OS loading -->
+{/if}
